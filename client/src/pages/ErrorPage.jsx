c528c7db246ce75b3291dc84b8460a1c1442c899
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-03-06 17:12:34 -0500

    added content to error Page

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -3,5 +3,20 @@ import { useRouteError } from 'react-router-dom';
 
 export default function ErrorPage() {
 
+    const { error } = useRouteError();
+    console.log(error);
+
+    return (
+        <div className="error">
+            <h1>👾 Uh Oh! Something went wrong... please refresh and try again! 👾</h1>
+            <br />
+            <div className="error-img">
+                <img src="https://media.giphy.com/media/3o7TKSjBzVNGz8d1tC/giphy.gif" alt="error" />
+            </div>
+            <br />
+            <p> 🖖 If this issue persists please see the detailed technical error below. Use this error as a referecne when communicating with support thank you 👽 </p>
+            <p>{error}</p>
+        </div>
+    );
 };
 
