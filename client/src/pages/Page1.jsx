b1d9336fa61f9609a4df52b3dfafe0c0769ea8b5
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-05-29 11:24:18 -0400

    Completed final touches on e-13 solution

--- client/src/pages/Page1.jsx
+++ client/src/pages/Page1.jsx
@@ -5,23 +5,8 @@ import { useStyle } from "../StyleContext";
 // Bootstrap Components
 import { Container, Row, Col, Card } from "react-bootstrap";
 
-const cardData = [
-  {
-    title: "UPDATE DATA SET 1 (TO BE MADE DYNAMIC)",
-    img: "https://media.giphy.com/media/DyQrKMpqkAhNHZ1iWe/giphy.gif?cid=82a1493boi0qtei6fk4053oolyjicm28pwm83lghonanq8dv&ep=v1_gifs_trending&rid=giphy.gif&ct=g",
-    text: "THIS IS DATA SET 1 (TO BE MADE DYNAMIC)",
-  },
-  {
-    title: "UPDATE DATA SET 2 (TO BE MADE DYNAMIC)",
-    img: "https://media.giphy.com/media/DyQrKMpqkAhNHZ1iWe/giphy.gif?cid=82a1493boi0qtei6fk4053oolyjicm28pwm83lghonanq8dv&ep=v1_gifs_trending&rid=giphy.gif&ct=h",
-    text: "THIS IS DATA SET 2 (TO BE MADE DYNAMIC)",
-  },
-  {
-    title: "UPDATE DATA SET 3 (TO BE MADE DYNAMIC)",
-    img: "https://media.giphy.com/media/DyQrKMpqkAhNHZ1iWe/giphy.gif?cid=82a1493boi0qtei6fk4053oolyjicm28pwm83lghonanq8dv&ep=v1_gifs_trending&rid=giphy.gif&ct=i",
-    text: "THIS IS DATA SET 3 (TO BE MADE DYNAMIC)",
-  },
-];
+// sample data for the cards
+import page1CardData from "../utils/page1CardData";
 
 export default function Page1() {
   const { activeStyle } = useStyle();
@@ -31,7 +16,7 @@ export default function Page1() {
         <Container className="text-center">
           <h1>🌠 Welcome to Caleus Framework! 🌠</h1>
           <Row>
-            {cardData.map((card, idx) => (
+            {page1CardData.map((card, idx) => (
               <Col md={4} key={idx}>
                 <Card>
                   <Card.Body>
