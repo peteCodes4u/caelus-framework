952d37807fa36fa1e14fc48d83d03f5e3e288aec
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-05-30 11:09:14 -0400

    completed e-14

--- client/src/components/ProfileForm/index.jsx
+++ client/src/components/ProfileForm/index.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import { Form, Button, Alert, Card } from 'react-bootstrap';
 import { useMutation, useQuery } from '@apollo/client';
 import { UPDATE_USER } from '../../utils/mutations';
 import { QUERY_ME } from '../../utils/queries';
@@ -54,62 +54,64 @@ export default function ProfileForm({ activeStyle = 'app-style1' }) {
     return (
         <div className={`${activeStyle}-profile-form`}>
             <div className={`${activeStyle}-prfile-form-header d-flex align-items-center justify-content-between mb-3`}>
-                <h4 className="mb-0">Profile</h4>
                 <div className={`${activeStyle}-profile-form-body`}>
-                    <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-                        <Alert
-                            dismissible
-                            onClose={() => setShowAlert(false)}
-                            show={showAlert || !!error}
-                            variant="danger"
-                        >
-                            Something went wrong with your profile update!
-                        </Alert>
-                        <Form.Group className="mb-3">
-                            <Form.Label htmlFor="username">Username</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Your username"
-                                name="name"
-                                id="name"
-                                value={userFormData.name}
-                                onChange={handleInputChange}
-                                required
-                            />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label htmlFor="email">Email</Form.Label>
-                            <Form.Control
-                                type="email"
-                                placeholder="Your email"
-                                name="email"
-                                id="email"
-                                value={userFormData.email}
-                                onChange={handleInputChange}
-                                required
-                            />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label htmlFor="password">Password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                placeholder="Your password"
-                                name="password"
-                                id="password"
-                                value={userFormData.password}
-                                onChange={handleInputChange}
-                                required
-                            />
-                        </Form.Group>
-                        {data ? (
-                            <Alert variant="success">
-                                Profile updated successfully!
+                    <Card className ={`${activeStyle}-profile-form-card`}>
+                        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+                            <Alert
+                                dismissible
+                                onClose={() => setShowAlert(false)}
+                                show={showAlert || !!error}
+                                variant="danger"
+                            >
+                                Something went wrong with your profile update!
                             </Alert>
-                        ) : null}
-                        <Button type="submit" variant="primary">
-                            Update Profile
-                        </Button>
-                    </Form>
+                            <Form.Group className="mb-3">
+                                <Form.Label htmlFor="username">Username</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Your username"
+                                    name="name"
+                                    id="name"
+                                    value={userFormData.name}
+                                    onChange={handleInputChange}
+                                    required
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label htmlFor="email">Email</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    placeholder="Your email"
+                                    name="email"
+                                    id="email"
+                                    value={userFormData.email}
+                                    onChange={handleInputChange}
+                                    required
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label htmlFor="password">Password</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    placeholder="Your password"
+                                    name="password"
+                                    id="password"
+                                    value={userFormData.password}
+                                    onChange={handleInputChange}
+                                    required
+                                />
+                            </Form.Group>
+                            {data ? (
+                                <Alert variant="success">
+                                    Profile updated successfully!
+                                </Alert>
+                            ) : null}
+                            <Button type="submit" variant="primary">
+                                Update Profile
+                            </Button>
+                        </Form>
+                    </Card>
+
                 </div>
             </div>
         </div>
