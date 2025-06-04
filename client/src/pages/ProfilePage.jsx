5ff57057f21326568b4b011d9066d8d46c6aa168
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-06-04 16:25:33 -0400

    completed changes to profilePage.jsx

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -2,15 +2,26 @@
 import { useParams } from "react-router-dom";
 import { useStyle } from "../StyleContext";
 import ProfileForm from "../components/ProfileForm";
+import { useQuery } from '@apollo/client';
+import { QUERY_ME } from '../utils/queries'; // Define this query
 
 export default function ProfilePage({ toggleStylesheet }) {
   const { id } = useParams();
   const { activeStyle } = useStyle();
 
+  const { data, loading, error } = useQuery(QUERY_ME, {
+    variables: { userId: id }
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error loading user.</p>;
+
+  const userName = data?.me?.name;
+
   return (
     <div className={`${activeStyle}-profile-page`}>
-      <h1>🛸 Welcome to your Profile Page! 🛸</h1>
-      <p>your userId is: {id}</p>
+      <h1>🛸 Hello! {userName} 🛸</h1>
+      <p> Welcome to your Profile Page! </p>
       <ProfileForm activeStyle={activeStyle} handleModalClose={toggleStylesheet} />
     </div>
   );
